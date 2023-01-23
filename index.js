322343218fa457f64931b5c0ee9779b2223ac3c6
Saidur Rahman <saidurhere@gmail.com>
2023-01-23 16:41:52 +0600

    Some updates

--- index.js
+++ index.js
@@ -21,7 +21,34 @@ app.post("/upload", upload.single("pdfFile"), async (req, res) => {
   // convert the first page of the pdf to an image
   pdfImage.convertPage(0).then(async (imagePath) => {
     // Recognize text from pdf file
-    const {
+
+    let text = "";
+    for (let i = 0; i < res.length; i++) {
+      const image = res[i];
+      const imageText = await Tesseract.recognize(image, {
+        lang: "urd+eng",
+      });
+      text += imageText;
+    }
+    const lines = text.split("\n");
+    const workbook = new Excel.Workbook();
+    const sheet = workbook.addWorksheet("Sheet 1");
+    for (let i = 0; i < lines.length; i++) {
+      const line = lines[i].trim();
+      if (line.length === 0) continue;
+      const row = line.split(" ");
+      sheet.addRow(row);
+    }
+    // Save the workbook to a file
+    workbook.xlsx.writeFile(`table.xlsx`).then(() => {
+      res.setHeader("Content-Disposition", "attachment; filename=table.xlsx");
+      res.setHeader(
+        "Content-Type",
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+      );
+      res.sendFile(table.xlsx, { root: "./" });
+    });
+    /*   const {
       data: { text },
     } = await Tesseract.recognize(imagePath, "eng", {
       tessjs_create_pdf: "1",
@@ -39,13 +66,13 @@ app.post("/upload", upload.single("pdfFile"), async (req, res) => {
     res.setHeader(
       "Content-Disposition",
       "attachment; filename=" + "table_data.xlsx"
-    );
+    ); */
     // Write the excel data to the response and send it to the client
-    workbook.xlsx.write(res).then(() => {
-      res
-        .status(200)
-        .send("PDF text extracted and excel file created successfully");
-    });
+    // workbook.xlsx.write(res).then(() => {
+    //   res
+    //     .status(200)
+    //     .send("PDF text extracted and excel file created successfully");
+    // });
   });
 });
 
