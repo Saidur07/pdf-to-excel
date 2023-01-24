d6a9fa1063e87d503a54189e6753b746b24ed787
Saidur Rahman <saidurhere@gmail.com>
2023-01-24 12:40:11 +0600

    Tesseracts codes updated

--- index.js
+++ index.js
@@ -1,76 +1,60 @@
 const express = require("express");
 const multer = require("multer");
-const PDFImage = require("pdf-image").PDFImage;
+const pdfImage = require("pdf-image").PDFImage;
+const pdfTableExtractor = require("pdf-table-extractor");
+const excel = require("exceljs");
 const Tesseract = require("tesseract.js");
-const Excel = require("exceljs");
-const pdf2excel = require("pdf-to-excel");
 const fs = require("fs");
-const path = require("path");
-
 const app = express();
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
-const upload = multer({ storage: storage });
+const upload = multer();
 
-app.post("/upload", upload.single("pdfFile"), async (req, res) => {
-  const newFilePath = `generated/${Math.round(Math.random() * 1000000)}.xlsx`
-  try {
-    const options = {
-      onProcess: (e) => console.warn(`${e.numPage} / ${e.numPages}`),
-      start: 1,
-    }
-    await pdf2excel.genXlsx(req.file.path, newFilePath, options)
-    console.log("Excel file created successfully");
-    res.status(200).sendFile(newFilePath, {
-      root: path.join(__dirname)
-    })
-  } catch {
-    res.status(500).send("Error");
+app.post("/upload", upload.single("pdf"), async (req, res) => {
+  if (fs.existsSync(req.file.path)) {
+    const pdf = new pdfImage(req.file.path);
+    await pdf.convertFile().then(async function (imagePaths) {
+      // Extract tables from images
+      imagePaths.forEach(async (imagePath) => {
+        await pdfTableExtractor(imagePath)
+          .then((tablesAsJson) => {
+            // Create a new Excel workbook
+            const workbook = new excel.Workbook();
+            const worksheet = workbook.addWorksheet("Tables");
+
+            // Add the tables to the worksheet
+            tablesAsJson.forEach((table) => {
+              worksheet.addRows(table);
+            });
+            // OCR
+            Tesseract.recognize(imagePath, { lang: "eng+urd" })
+              .progress(function (p) {
+                console.log("progress", p);
+              })
+              .catch((err) => console.error(err))
+              .then(function (result) {
+                console.log(result.text);
+              });
+            // Send the Excel file to the client
+            res.setHeader(
+              "Content-Type",
+              "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            );
+            res.setHeader(
+              "Content-Disposition",
+              "attachment; filename=tables.xlsx"
+            );
+            return workbook.xlsx.write(res);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
+    });
+  } else {
+    console.log("File not found");
   }
 });
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
-
-
-        // const pdfImage = new PDFImage(req.file.path);
-      // // convert the first page of the pdf to an image
-      // pdfImage.convertPage(0).then(async (imagePath) => {
-      //   // Recognize text from pdf file
-
-      //   const {
-      //     data: { text },
-      //   } = await Tesseract.recognize(imagePath, "eng", {
-      //     tessjs_create_pdf: "1",
-      //   });
-      //   console.log(data);
-      //   // Create a new excel workbook
-      //   const workbook = new Excel.Workbook();
-      //   const worksheet = workbook.addWorksheet("Table Data");
-      //   // Add the text to the worksheet
-      //   worksheet.addRow([text]);
-      //   // Set the response headers
-      //   res.setHeader(
-      //     "Content-Type",
-      //     "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-      //   );
-      //   res.setHeader(
-      //     "Content-Disposition",
-      //     "attachment; filename=" + "table_data.xlsx"
-      //   );
-      //   // Write the excel data to the response and send it to the client
-      //   workbook.xlsx.write(res).then(() => {
-      //     console.log("Excel file created successfully");
-      //     res
-      //       .status(200)
-      //       .send("PDF text extracted and excel file created successfully");
-      //   });
-      // });
